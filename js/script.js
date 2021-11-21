db4119628e1decffae7c697772787b025517f0cc
Alexis Dela Torre <alexisndelatorre@gmail.com>
2021-11-22 07:29:10 +0800

    Initial Commit
    Includes html css and js files for the project

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// 1. We define the chart options. 
+const options =  {
+    chart: {
+        type: 'line',
+        height:"50%"
+    },
+    // each series represents one set of data
+    series:[
+        {
+            name: 'sightings',
+            data:[10, 13, 15, 22, 34, 23, 55, 78, 44]
+        }
+    ],
+    // what is are the labels along the x-axis (horizontal line)
+    xaxis: {
+        categories:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct']
+    },
+    
+}
+ 
+// create the chart
+const chart = new ApexCharts(document.querySelector('#chart'), options);
+ 
+// render the chart
+chart.render()
